1b7f176f10a21e7f767a9f4af75dca0ba11edabe
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-10-03 21:01:46 +0200

    Complete exercises folder

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let total = 0;
+console.log(`QTY    ITEM    TOTAL`);
+order.forEach(({ itemName, quantity, unitPrice }) => {
+  total = total + unitPrice;
+
+  console.log(`${quantity}    ${itemName}   ${unitPrice.toFixed(2)}`);
+});
+console.log(`
+Total: ${total}
+`);
